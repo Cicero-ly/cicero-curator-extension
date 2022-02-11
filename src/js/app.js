00d8b2bf20ab6fedc4c1224087106a7c98e5a9de
Tyler Termini <tntermini@gmail.com>
2022-02-10 22:33:15 -0800

    shortcuts uses action API, not commands API, and opens popup

---
+++ src/js/app.js
@@ -0,0 +1,38 @@
+// todo: this isnt working
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log('dom loaded');
+})
+
+async function addCustomThought(url) {
+    const [curatorEmail, curatorPass] = await getAuthStatus();
+    const request = `https://dev-api.cicero.ly/admin/curator/thoughts?curatorEmail=${curatorEmail}&curatorPass=${curatorPass}&pass=AyEDyX%268%26YXx8qhSuX2%23TGURb9SAjxkMjpfG!%5E6aCcg%23H3!dTwCuPoDD%23dMWvJN6%40S%24Je`
+    const requestBody = {
+            newThought: true,
+            url: url
+    }
+    let res = await fetch(request, {
+        method: "post",
+        body: new Blob([JSON.stringify(requestBody)], {type : 'application/json'})
+    });
+    // just for testing...
+    // const request = `https://dev-api.cicero.ly/admin/curator/fetch-meta-tags?url=${url}&curatorEmail=${curatorEmail}&curatorPass=${curatorPass}&pass=AyEDyX%268%26YXx8qhSuX2%23TGURb9SAjxkMjpfG!%5E6aCcg%23H3!dTwCuPoDD%23dMWvJN6%40S%24Je`;
+    // let res = await fetch(request, {
+    //     method: "get",
+    // });
+    // return res.json();
+    if (res.status !== 200) {
+        // throw...
+    }
+    return undefined;
+}
+
+function getAuthStatus() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get('ciceroUser', res => {
+            if (!res) {
+                reject([undefined, undefined]);
+            }
+            resolve([res.ciceroUser.email, res.ciceroUser.password]);
+        })
+    });
+}
