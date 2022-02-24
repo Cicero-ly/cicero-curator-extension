4ebdaa6fc9dbae6d9cc6366bbab85b9052861e3c
Tyler Termini <tntermini@gmail.com>
2022-02-23 16:14:14 -0800

    hide login form if already logged in

--- js/app.js
+++ js/app.js
@@ -1,72 +1,83 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+main();
 
+function main() {
+  document.addEventListener("DOMContentLoaded", () => {
     // Have to use full tabs permissions (instead of the less-invasive "activeTab" permission)
     // because activeTab can only be used as an argument when doing chrome.action.onClicked,
     // (among a few other apis which don't suit my case)
-    // which frustratingly doesn't work well with showing a popup with a keyboard shortcut. 
+    // which frustratingly doesn't work well with showing a popup with a keyboard shortcut.
     // So using the "DOMContentLoaded" listener with a full tabs query is the kludge for now.
     // (using the DOMContentLoaded method for performing an action after popup appears was
-    // recommended in chrome docs) 
+    // recommended in chrome docs)
     let currentTabUrl;
-    chrome.tabs.query({
+    chrome.tabs.query(
+      {
         active: true,
-        currentWindow: true
-    }, (tabs) => {
-        document.getElementById("status_msg").innerHTML = "Adding custom URL...";
+        currentWindow: true,
+      },
+      (tabs) => {
+        // todo: actually figure out how modules work so this doesn't get executed when options.html loads
+        document.getElementById("status_msg").innerHTML =
+          "Adding custom URL...";
         currentTabUrl = tabs[0]["url"];
 
         addCustomThought(currentTabUrl)
-        .then(res => {
+          .then((res) => {
             if (res.status && res.status === 200) {
-                console.log(res.message_verbose);
-                document.getElementById("status_msg").style.color = "forestgreen";
-                document.getElementById("status_msg").innerHTML = res.message;
-            } 
-        })
-        .catch(e => {
+              console.log(res.message_verbose);
+              document.getElementById("status_msg").style.color = "forestgreen";
+              document.getElementById("status_msg").innerHTML = res.message;
+            }
+          })
+          .catch((e) => {
             console.error(e);
             document.getElementById("status_msg").style.color = "red";
             document.getElementById("status_msg").innerHTML = e;
-        })
-    });
-});
-
-async function addCustomThought(url) {
-    console.log(url);
-    let urlRegex = /(((http[s]?:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
-    let urlMatch;
-    if (url) {
-        urlMatch = url.match(urlRegex);
-    }
-    if (!urlMatch) {
-        throw "Not a valid URL."
-    }
-
-    const [curatorEmail, curatorPass] = await getAuthStatus();
-
-    const request = `https://api.cicero.ly/admin/curator/thoughts?curatorEmail=${curatorEmail}&curatorPass=${curatorPass}&extensionPass=9saGbMoDek4yLjQKJfqyh9fAgAdKhwH8HQX8LUjh6pQjsuVdPt`
-    const requestBody = {
-            newThought: true,
-            url: url
-    }
-    let res = await fetch(request, {
-        method: "post",
-        body: new Blob([JSON.stringify(requestBody)], {type : 'application/json'})
-    });
-    if (res.status !== 200) {
-        console.error(res);
-        throw { status: res.status, error_message: res.message_verbose }
-    }
-    return res.json();
+          });
+      }
+    );
+  });
 }
 
-function getAuthStatus() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.sync.get(['ciceroUser'], res => {
-            if (!res.ciceroUser || Object.keys(res.ciceroUser)?.length === 0) {
-                reject('Not authenticated. Please right-click the extension icon, click "options", and login.');
-            }
-            resolve([res.ciceroUser.email, res.ciceroUser.password]);
-        })
+async function addCustomThought(url) {
+  console.log(url);
+  let urlRegex =
+    /(((http[s]?:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+  let urlMatch;
+  if (url) {
+    urlMatch = url.match(urlRegex);
+  }
+  if (!urlMatch) {
+    throw "Not a valid URL.";
+  }
+
+  const [curatorEmail, curatorPass] = await getAuthStatus();
+
+  const request = `https://api.cicero.ly/admin/curator/thoughts?curatorEmail=${curatorEmail}&curatorPass=${curatorPass}&extensionPass=9saGbMoDek4yLjQKJfqyh9fAgAdKhwH8HQX8LUjh6pQjsuVdPt`;
+  const requestBody = {
+    newThought: true,
+    url: url,
+  };
+  let res = await fetch(request, {
+    method: "post",
+    body: new Blob([JSON.stringify(requestBody)], { type: "application/json" }),
+  });
+  if (res.status !== 200) {
+    console.error(res);
+    throw { status: res.status, error_message: res.message_verbose };
+  }
+  return res.json();
+}
+
+export function getAuthStatus() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(["ciceroUser"], (res) => {
+      if (!res.ciceroUser || Object.keys(res.ciceroUser)?.length === 0) {
+        reject(
+          'Not authenticated. Please right-click the extension icon, click "options", and login.'
+        );
+      }
+      resolve([res.ciceroUser.email, res.ciceroUser.password]);
     });
+  });
 }
--- js/auth.js
+++ js/auth.js
@@ -1,57 +1,80 @@
-let form = document.getElementById("authForm");
+import { getAuthStatus } from "./app.js";
+
+let form = document.getElementById("auth_form");
+getAuthStatus()
+.then(data => {
+    showAuthMessage(true);
+}).catch((error) => {
+  showAuthMessage(false);
+});
+
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    document.getElementById("auth_status").innerHTML = "waiting...";
-    
-    let curatorEmailInput = document.getElementById("curatorEmailInput").value;
-    let curatorPassInput = document.getElementById("curatorPassInput").value;
-    
-    let authRequest = `https://api.cicero.ly/admin/curator/get-auth-status?curatorEmail=${curatorEmailInput}&curatorPass=${curatorPassInput}&extensionPass=9saGbMoDek4yLjQKJfqyh9fAgAdKhwH8HQX8LUjh6pQjsuVdPt`;
-    fetch(authRequest, { method: "get" })
-    .then(res => {
-        if (res.ok) {
-            res.json()
-            .then(body => console.log(body));
+  document.getElementById("auth_status").innerHTML = "waiting...";
 
-            setLocalStorage({
-                email: curatorEmailInput,
-                password: curatorPassInput
-            });
+  let curatorEmailInput = document.getElementById("curatorEmailInput").value;
+  let curatorPassInput = document.getElementById("curatorPassInput").value;
 
-            document.getElementById("auth_status").style.color = "forestgreen";
-            document.getElementById("auth_status").innerHTML = "Successfully logged in";
-        }
+  let authRequest = `https://api.cicero.ly/admin/curator/get-auth-status?curatorEmail=${curatorEmailInput}&curatorPass=${curatorPassInput}&extensionPass=9saGbMoDek4yLjQKJfqyh9fAgAdKhwH8HQX8LUjh6pQjsuVdPt`;
+  fetch(authRequest, { method: "get" })
+    .then((res) => {
+      if (res.ok) {
+        res.json().then((body) => console.log(body));
+
+        setLocalStorage({
+          email: curatorEmailInput,
+          password: curatorPassInput,
+        });
+        showAuthMessage(true);
+      }
     })
-    .catch(e => {
-        document.getElementById("auth_status").style.color = "red";
-        document.getElementById("auth_status").innerHTML = "Authentication failed";
-        console.error(e);
-        setLocalStorage({});
-    })
-})
+    .catch((e) => {
+      showAuthMessage(false);
+      console.error(e);
+      setLocalStorage({});
+    });
+});
 
 function setLocalStorage(obj) {
-    if (Object.keys(obj).length === 0) {
-        chrome.storage.sync.set({ciceroUser: obj }, () => {
-            console.log('localStorage cleared');
-            chrome.storage.sync.get(['ciceroUser'], data => {
-                console.log(data);
-            })
-        })
-        return;
-    }
+  if (Object.keys(obj).length === 0) {
     chrome.storage.sync.set({ ciceroUser: obj }, () => {
-        console.log('localStorage successfully set');
-        chrome.storage.sync.get(['ciceroUser'], data => {
-            console.log(data);
-        })
-    })
+      console.log("localStorage cleared");
+      chrome.storage.sync.get(["ciceroUser"], (data) => {
+        console.log(data);
+      });
+    });
+    return;
+  }
+  chrome.storage.sync.set({ ciceroUser: obj }, () => {
+    console.log("localStorage successfully set");
+    chrome.storage.sync.get(["ciceroUser"], (data) => {
+      console.log(data);
+    });
+  });
 }
 
 let shortcutLink = document.getElementById("shortcut-link");
-shortcutLink.addEventListener("click", event => {
-    chrome.tabs.create({
-        url: 'chrome://extensions/shortcuts'
-    });
-})
+shortcutLink.addEventListener("click", (event) => {
+  chrome.tabs.create({
+    url: "chrome://extensions/shortcuts",
+  });
+});
+
+function showAuthMessage(status) {
+  switch (status) {
+    case true:
+      document.getElementById("auth_form").style.display = "none";
+      document.getElementById("auth_status").style.color = "forestgreen";
+      document.getElementById("auth_status").innerHTML =
+        "Successfully logged in";
+      break;
+    case false:
+      document.getElementById("auth_status").style.color = "red";
+      document.getElementById("auth_status").innerHTML =
+        "Authentication failed";
+      break;
+    default:
+      break;
+  }
+}
